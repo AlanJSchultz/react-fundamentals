54269469ae2ea39fa9c47894686a97765950e946
AlanJschultz <alanjschultz@gmail.com>
2020-01-01 23:37:02 -0500

    01012020 react practice modules

--- src/javascript_practice/2_constructors.js
+++ src/javascript_practice/2_constructors.js
@@ -29,3 +29,20 @@ console.log(carOne.mk);  // Chrysler
 console.log(carOne.md);  // Town and Country
 console.log(carOne.yr); // 2001
 console.log(carOne); // Car { mk: 'Chrysler', md: 'Town and Country', yr: '2001' }
+
+
+// practice - write a Player class with three properties of name, number, and average points.
+
+class Player {
+    constructor (name, number, averagePoints) {
+        this.name = name;
+        this.number = number;
+        this.averagePoints = averagePoints;
+    }
+}
+var playerOne = new Player("Woods", 1, 66);
+console.log(playerOne.name);  // Woods
+console.log(playerOne.number);  // 1
+console.log(playerOne.averagePoints);  // 66
+console.log(playerOne); // Player { name: 'Woods', number: 1, averagePoints: 66 }
+
--- src/javascript_practice/3_extends.js
+++ src/javascript_practice/3_extends.js
@@ -1,2 +1,52 @@
 //jshint esversion:6
 
+// ES6 JS Classes
+class User {
+    constructor (name) {
+        this.name = name;
+        this.type = "Trial User";
+    }
+    // Method 1
+    greet() {
+        console.log('Welcome back, ' + this.name);
+    }
+    // Method 2
+    status() {
+        console.log('Current Status: ' + this.type);
+    }
+}
+
+//Extends
+class TrialUser extends User {
+    trialEnding () {
+        console.log('Your trial will be ending soon, ' + this.name + '.' + ' Would you like to join our program?');
+    }
+}
+
+//Instance of User Class
+var anonDude = new User("Anonymous");
+anonDude.greet();
+anonDude.status();
+
+//Instance of TrialUser class
+var trialUser = new TrialUser("Paul");
+trialUser.greet();
+trialUser.trialEnding();
+trialUser.status();
+
+
+class BannedUser {
+    constructor (name) {
+        this.name = name;
+        this.type = "Banned for conduct.";
+    }
+    greet () {
+        console.log("No longer welcomed, " + this.name);
+    }
+    status () {
+        console.log("Current Status: " + this.type);
+    }
+}
+var badDude = new BannedUser("Bad Dude");
+badDude.greet();
+badDude.status();
